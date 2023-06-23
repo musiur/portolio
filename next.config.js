b9aa51480ba5f3f9b46ca87317ca993ad4d80f6b
Musiur Alam Opu <musiur.opu@gmail.com>
2023-06-23 23:00:55 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-   HOST: "http://localhost:3000",
-    HOST_BACKEND: "http://localhost:8080",
-    // HOST: "https://musiur.vercel.app/",
-    // HOST_BACKEND: "https://bewildered-fox-overshirt.cyclic.app/",
+  // env: {
+  //  HOST: "http://localhost:3000",
+  //  HOST_BACKEND: "http://localhost:8080",
+    HOST: "https://musiur.vercel.app/",
+    HOST_BACKEND: "https://bewildered-fox-overshirt.cyclic.app/",
   },
 };
 
