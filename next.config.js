5af9e272365f47e37268a9e6bd88b2c24ae8b58e
Musiur Alam Opu <musiur.opu@gmail.com>
2023-06-23 23:02:00 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // env: {
+  env: {
   //  HOST: "http://localhost:3000",
   //  HOST_BACKEND: "http://localhost:8080",
     HOST: "https://musiur.vercel.app/",
