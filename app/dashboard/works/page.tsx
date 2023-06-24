a1e588bba595f313500c06f2890b7c353966260e
musiur <musiur.opu@gmail.com>
2023-06-24 14:49:13 +0600

    comp-dev:: dashboard: read and edit state added

--- app/dashboard/works/page.tsx
+++ app/dashboard/works/page.tsx
@@ -1,18 +1,25 @@
 'use client'
 
 import DashboardNav from "@/components/base/DashboardNav";
+import Edit from "@/components/pages/dashboard/works/Edit";
+import Read from "@/components/pages/dashboard/works/Read";
 import PrivateRoute from "@/routes/PrivateRoute";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Works = () => {
   useEffect(() => {
     localStorage.setItem("from", "/dashboard/works");
-  }, [])
+  }, []);
+   const [edit, setEdit] = useState(false);
   return (
     <PrivateRoute>
       <div>
         <DashboardNav />
-        <div className="p-5 md:p-10">Works</div>
+        <div className="p-5 md:p-10">
+          {
+            !edit ? <Read /> : <Edit />
+          }
+        </div>
       </div>
     </PrivateRoute>
   );
